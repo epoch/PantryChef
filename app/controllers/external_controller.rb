faca88b36b25c98a6d5e5e15d89b07abe13aac9c
Jam Creencia <jrcreencia@gmail.com>
2014-10-31 10:23:42 +1100

    fix http

--- app/controllers/external_controller.rb
+++ app/controllers/external_controller.rb
@@ -4,6 +4,9 @@ class ExternalController < ApplicationController
 		@recipe_id = params['id']
 		@recipe = HTTParty.get("http://api.yummly.com/v1/api/recipe/#{@recipe_id}?_app_id=8bf9c732&&_app_key=b14d7c43eeb5c83d0c62bcfebc042431")
 		@external_url = @recipe['source']['sourceRecipeUrl']
+		@external_url[5..-1]
+
+
 	end
 
 
