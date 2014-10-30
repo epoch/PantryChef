621044457e048fa89069e710fa41b2d8497d78d1
Jam Creencia <jrcreencia@gmail.com>
2014-10-31 10:11:03 +1100

    iframe

--- app/controllers/external_controller.rb
+++ app/controllers/external_controller.rb
@@ -1,9 +1,15 @@
 class ExternalController < ApplicationController
-	
+	after_action :allow_iframe, only: :index
 	def index
 		@recipe_id = params['id']
 		@recipe = HTTParty.get("http://api.yummly.com/v1/api/recipe/#{@recipe_id}?_app_id=8bf9c732&&_app_key=b14d7c43eeb5c83d0c62bcfebc042431")
 		@external_url = @recipe['source']['sourceRecipeUrl']
+	end
+
+
+	private
+	def allow_iframe
+  response.headers['X-Frame-Options'] = "ALLOWALL"
 	end
 
 end
