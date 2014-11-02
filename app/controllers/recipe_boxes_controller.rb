410f0e24b6d69a3fcba18423422c5f5969da4d8d
Jam Creencia <jrcreencia@gmail.com>
2014-11-02 11:22:48 +1100

    refactor recipe box controller

--- app/controllers/recipe_boxes_controller.rb
+++ app/controllers/recipe_boxes_controller.rb
@@ -26,24 +26,9 @@ class RecipeBoxesController < ApplicationController
 		recipe = HTTParty.get("http://api.yummly.com/v1/api/recipe/#{@recipe_id}?_app_id=8bf9c732&&_app_key=b14d7c43eeb5c83d0c62bcfebc042431")
 		recipe = recipe.parsed_response
 
-		@recipe = Recipe.new
-		@recipe.image_url = recipe["images"].first["imageUrlsBySize"]['90']
-		@recipe.name = recipe['name']
-		@recipe.rating = recipe['rating']
-		@recipe.cooktime = recipe['totalTimeInSeconds'].to_i / 60
-		@recipe.course = if recipe['attributes']['course'] != nil 
-												recipe['attributes']['course'].first
-											else
-												"---"
-											end
-		@recipe.ingredients = recipe['ingredientLines'].join(', ')
-		@recipe.yummly_id = recipe['id']
-		@recipe.recipe_boxes << @recipe_box
-		@recipe.save
+		new_recipe(recipe)
 		
-
-		
-			redirect_to recipe_box_path(@recipe_box)
+		redirect_to recipe_box_path(@recipe_box)
 		
 	end
 
@@ -54,24 +39,9 @@ class RecipeBoxesController < ApplicationController
 		recipe = HTTParty.get("http://api.yummly.com/v1/api/recipe/#{@recipe_id}?_app_id=8bf9c732&&_app_key=b14d7c43eeb5c83d0c62bcfebc042431")
 		recipe = recipe.parsed_response
 
-		@recipe = Recipe.new
-		@recipe.image_url = recipe["images"].first["imageUrlsBySize"]['90']
-		@recipe.name = recipe['name']
-		@recipe.rating = recipe['rating']
-		@recipe.cooktime = recipe['totalTimeInSeconds'].to_i / 60
-		@recipe.course = if recipe['attributes']['course'] != nil 
-												recipe['attributes']['course'].first
-											else
-												"---"
-											end
-		@recipe.ingredients = recipe['ingredientLines'].join(', ')
-		@recipe.yummly_id = recipe['id']
-		@recipe.recipe_boxes << @recipe_box
-		@recipe.save
+		new_recipe(recipe)
 		
-
-		
-			redirect_to recipe_box_path(params['recipe_box'])
+		redirect_to recipe_box_path(params['recipe_box'])
 
 	end
 
@@ -114,6 +84,25 @@ class RecipeBoxesController < ApplicationController
 
 		redirect_to recipe_boxes_path
 		
+	end
+
+	def new_recipe(recipe)
+
+		@recipe = Recipe.new
+		@recipe.image_url = recipe["images"].first["imageUrlsBySize"]['90']
+		@recipe.name = recipe['name']
+		@recipe.rating = recipe['rating']
+		@recipe.cooktime = recipe['totalTimeInSeconds'].to_i / 60
+		@recipe.course = if recipe['attributes']['course'] != nil 
+												recipe['attributes']['course'].first
+											else
+												"---"
+											end
+		@recipe.ingredients = recipe['ingredientLines'].join(', ')
+		@recipe.yummly_id = recipe['id']
+		@recipe.recipe_boxes << @recipe_box
+		@recipe.save
+
 	end
 
 
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -33,6 +33,7 @@ class RecipesController < ApplicationController
 		url = URI.encode("http://api.yummly.com/v1/api/recipes?_app_id=8bf9c732&_app_key=b14d7c43eeb5c83d0c62bcfebc042431#{allowedIngredient}#{allowedAllergy}&requirePictures=true&maxResult=12")
 		@response = HTTParty.get(url)
 		@matches = @response['matches']
+
 		@total_match_count = @response['totalMatchCount']
 		
 		render :index
@@ -79,5 +80,8 @@ class RecipesController < ApplicationController
 		selected_recipe.save
 		redirect_to recipe_path(recipe_id)
 	end
+
+
+	
 
 end
